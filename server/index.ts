013ccb4832ed3aae89ff7594072542bf6c3fd0f8
Beanalini <dat826@gmail.com>
2023-12-22 20:50:25 +0000

    add server base project structure

---
+++ server/index.ts
@@ -0,0 +1,62 @@
+import * as express from "express";
+import * as cors from "cors";
+import { Server } from "http";
+import { initialiseRoutes } from "./routes/routes";
+import { printNewLine } from "./helpers/helpers";
+
+const PORT = 8080;
+
+try {
+  printNewLine();
+
+  console.log("💫 Initialising Server...");
+  const app = express();
+
+  console.log("👉 Enabling JSON middleware...");
+  app.use(express.json());
+
+  console.log("👉 Enabling URL-Encoded middleware...");
+  app.use(express.urlencoded({ extended: true }));
+
+  console.log("👉 Enabling CORS...");
+  app.use(cors());
+
+  initialiseRoutes(app);
+
+  const server = app
+    .listen(PORT, () => {
+      console.log(
+        `🍸 CocktailChords Server is now listening on port: ⚓ ${PORT} 🍸`
+      );
+      printNewLine();
+      console.log(
+        `🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸`
+      );
+      console.log(
+        `🍸    Health check at "http://localhost:${PORT}/health"            🍸`
+      );
+
+      console.log(
+        `🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸🍸`
+      );
+    })
+    .on("error", (error) => {
+      console.error("🚨 Express Error Handler 🚨 ");
+      console.error(error);
+    });
+
+  process.on("SIGINT", () => handleShutdown(server));
+  process.on("SIGTERM", () => handleShutdown(server));
+  process.on("SIGHUP", () => handleShutdown(server));
+} catch (e: unknown) {
+  console.error("🚨 Top level Error caught 🚨 ");
+  console.error((e as Error).message);
+}
+
+function handleShutdown(server: Server) {
+  console.log("🚪 Closing Server...");
+  server.close(() => {
+    console.log("🏥 Express server closed ✅");
+    process.exit(0);
+  });
+}
---
+++ server/routes/routes.ts
@@ -0,0 +1,45 @@
+import * as express from "express";
+import { Express } from "express";
+
+export function initialiseRoutes(app: Express) {
+  console.log("🏗️  Setting up routers...");
+
+  addHealthCheck(app);
+
+  addAPIRoutes(app);
+}
+
+function addHealthCheck(app: Express) {
+  console.log("🛠️  Creating base router...");
+
+  const baseRouter = express.Router();
+
+  baseRouter.use((req, res, next) => {
+    res.header("Access-Control-Allow-Methods", "GET");
+    console.log(`📨 ${req.url}`);
+    next();
+  });
+
+  console.log("🏠❤️‍🩹  Adding health check route...");
+  baseRouter.get("/health", (req, res) => {
+    res.status(200).send("👍 Okay! The server is responding! 🙌");
+  });
+
+  console.log("🛠️  Applying base router to Express server...");
+  app.use("/", baseRouter);
+}
+
+// this function adds all the routes we can access by going to /api/[someRoute]
+function addAPIRoutes(app: Express) {
+  console.log("🛠️  Creating API router...");
+
+  const apiRouter = express.Router();
+  apiRouter.use((req, res, next) => {
+    // we'll use this router to return specifically JSON
+    res.setHeader("Content-Type", "application/json");
+    next();
+  });
+
+  console.log("🛠️  Applying API router to Express server...");
+  app.use("/api", apiRouter);
+}
