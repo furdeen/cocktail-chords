fe7e7807c61419f922918848201b5438dfaab362
Ben <manglebot@users.noreply.github.com>
2024-01-02 12:27:21 +0000

    feat:add pages

---
+++ client/src/components/router/router.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "../home/home";
+// import NotFound from "../not_found/not_found";
+import Dailymix from "../dailymix/dailymix";
+
+import MainLayout from "../layouts/main_layouts";
+import Moviemix from "../moviemix/moviemix";
+import NotFound from "../not_found/not_found";
+
+const Router: React.FC = () => (
+  <Routes>
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="dailymix" element={<Dailymix />} />
+      <Route path="moviemix" element={<Moviemix />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+);
+
+export default Router;
