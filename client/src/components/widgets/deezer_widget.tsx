b7f30d6618f7f92b2b7c7ae02d1e9ea245a962da
Ben <manglebot@users.noreply.github.com>
2024-01-03 17:31:57 +0000

    feat:separate ingredients and measures to format properly

--- client/src/components/widgets/deezer_widget.tsx
+++ client/src/components/widgets/deezer_widget.tsx
@@ -5,20 +5,27 @@ type DeezerWidgetProps = {
 };
 
 const DeezerWidget: React.FC<DeezerWidgetProps> = ({ trackId }) => {
-  const [deezerWidget, setDeezerWidget] = useState<string | null>(null);
+  const [deezerWidget, setDeezerWidget] = useState<string | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     if (trackId) {
       setDeezerWidget(`https://widget.deezer.com/widget/dark/track/${trackId}`);
+    } else {
+      // play Daft Punk if nothing else is available
+      setDeezerWidget(`https://widget.deezer.com/widget/dark/track/3135556`);
     }
   }, [trackId]);
 
   return (
     <div className="daily-mix__container">
       <iframe
+        className="deezer__iframe"
         title="deezer-widget"
         src={deezerWidget}
         width="100%"
+        frameBorder="0"
         height="300"
         allow="encrypted-media; clipboard-write"
       ></iframe>
